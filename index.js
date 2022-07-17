faa0f2cb8b9db1ac254e781f6041dbf51356ec17
CPTRICODEV <97399891+CPTRICODEV@users.noreply.github.com>
2022-07-17 22:49:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function() {
     document.getElementById('loadingScreen').style.visibility = 'hidden';
     document.getElementById('information').style.visibility = 'visible';
+    document.getElementById('information').style.overflow = 'hidden';
 })
