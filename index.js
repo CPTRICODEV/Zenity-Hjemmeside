13339caff854baa14c08b1ccd0529745b8cf5b46
CPTRICODEV <97399891+CPTRICODEV@users.noreply.github.com>
2022-07-17 22:44:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', function() {
-    //document.getElementById('loadingScreen').style.visibility = 'hidden';
+    document.getElementById('loadingScreen').style.visibility = 'hidden';
     document.getElementById('information').style.visibility = 'visible';
 })
